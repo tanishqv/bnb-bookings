fbd8907aae1df60369527bab079d6933a2041ad6
tanishqv <tanishq7verma@gmail.com>
2022-12-07 15:02:26 +0530

    creating go templates from HTML source

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -33,20 +33,45 @@ func NewHandlers(r *Repository) {
 
 // Home is the home page handler
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	// remoteIP := r.RemoteAddr
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
 // About is the about page handler
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
-	stringMap := make(map[string]string)
-	stringMap["test"] = "Hello, again."
+	// stringMap := make(map[string]string)
+	// stringMap["test"] = "Hello, again."
 
-	stringMap["remote_ip"] = m.App.Session.GetString(r.Context(), "remote_ip")
+	// stringMap["remote_ip"] = m.App.Session.GetString(r.Context(), "remote_ip")
 
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
-		StringMap: stringMap,
+		// StringMap: stringMap,
 	})
+}
+
+// Generals renders the room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Colonels renders the room page
+func (m *Repository) Colonels(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "colonels.page.tmpl", &models.TemplateData{})
+}
+
+// Reservation renders the make a reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// Availability renders the search availability form
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// Contact renders the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
 }
