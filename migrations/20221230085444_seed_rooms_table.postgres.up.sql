339d605a042cdcb0d2a1a27e3217547d03ee43a2
tanishqv <tanishq7verma@gmail.com>
2023-01-12 13:42:03 +0530

    correcting the room name from major's suite to colonel's suite

--- migrations/20221230085444_seed_rooms_table.postgres.up.sql
+++ migrations/20221230085444_seed_rooms_table.postgres.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO rooms ("id", "room_name", "created_at", "updated_at") VALUES
 (1, 'General''s Quarters', '2022-12-29 00:00:00', '2022-12-29 00:00:00'),
-(2, 'Major''s Suite', '2022-12-29 00:00:00', '2022-12-29 00:00:00');
+(2, 'Colonel''s Suite', '2022-12-29 00:00:00', '2022-12-29 00:00:00');
